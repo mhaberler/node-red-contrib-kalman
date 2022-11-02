957b40d745d96a36d414f83125ad83247940dd1b
Michael Haberler <git@mah.priv.at>
2022-11-02 12:33:52 +0100

    add msg.processNoise and msg.measurementNoise property handling to set params at runtime

--- kalman/kalman.js
+++ kalman/kalman.js
@@ -1,4 +1,4 @@
-var KalmanFilter = require('kalmanjs')
+var KalmanFilter = require('kalmanjs');
 
 module.exports = function(RED) {
     function KalmanFilterType(config) {
@@ -6,6 +6,12 @@ module.exports = function(RED) {
         var kalmanFilter = new KalmanFilter({R: config.R || 0.01, Q: config.Q || 3});
         var node = this;
         this.on('input', function(msg) {
+            if ('processNoise' in msg) {
+                kalmanFilter.setProcessNoise(msg.processNoise);
+            }
+            if ('measurementNoise' in msg) {
+                setMeasurementNoise.setProcessNoise(msg.measurementNoise);
+            }
             if(msg.payload instanceof Array) {
                 msg.payload = msg.payload.map(function(v) {
                     return kalmanFilter.filter(v);
@@ -17,4 +23,4 @@ module.exports = function(RED) {
         });
     }
     RED.nodes.registerType("kalman-filter", KalmanFilterType);
-}
+};
